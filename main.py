c129466c3c6818e9ceb485370d9c0825f5e5ca82
macmay91 <macmay91@gmail.com>
2024-10-21 11:03:24 -0400

    setup player script to draw a triangle with circlular hitbox

--- main.py
+++ main.py
@@ -3,20 +3,32 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player_ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player_ship.draw(screen)
         pygame.display.flip()
+
+        clock.tick(60)
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
